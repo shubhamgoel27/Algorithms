8d8bc31cee4f604c86e7ac128d5779dfccb13c01
shubhamgoel27 <shubhamgoel27@gmail.com>
2015-02-03 01:16:10 +0530

    Added Chebychev's method code

--- Numerical Methods/Chebychev_method.py
+++ Numerical Methods/Chebychev_method.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jan 31 15:51:41 2015
+Created on Tue Feb  3 00:58:22 2015
 
 @author: shubham
 """
@@ -25,17 +25,25 @@ def derivative(y):
     '''
     return y.diff(x)
 
-def newton_raphson(x0 = 2, function = x**2 -1):
+def second_derivative(y):
+    '''Returns derivative of the function
+    '''
+    first = y.diff(x)
+    return first.diff(x)
+    
+def chebychev(x0 = 2, function = x**2 -1):
     ''' Returns the root of the input function
         x0 is the initial guess
-        
     '''
-    #print 'Starting Newton Raphson'
+    #print 'Starting Chebychev'
     while abs(fn(x0, function)) > TOLERANCE:
         fx_n = fn(x0, function)
         fprime_n = derivative(function).subs(x,x0)
-        x0 = x0 - (fx_n/fprime_n)            
+        
+        f2prime_n = second_derivative(function).subs(x, x0)
+        x0 = x0 - (fx_n/fprime_n) #Left part
+        x0 = x0 - (1/2)* ((fx_n/fprime_n)**2)* (f2prime_n/fx_n)       
     return x0    
     
-print newton_raphson()  
     
+print N(chebychev())
